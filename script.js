7cdd8f76fab7794f13acee96f735d45d5f59cd27
saviosajan <74667758+saviosajan@users.noreply.github.com>
2020-12-12 23:25:47 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+var s;
+var ss;
+var sss;
+function ss1()
+{
+  if(s==1)
+  {
+    document.getElementById("s1").style.display="inline";
+    return s=0;
+  }
+  else
+  {
+    document.getElementById("s1").style.display="none";
+    return s=1;
+  }
+}
+function ss2()
+{
+  if(s==1)
+  {
+    document.getElementById("s2").style.display="inline";
+    return ss=0;
+  }
+  else
+  {
+    document.getElementById("s2").style.display="none";
+    return ss=1;
+  }
+}
+function ss3()
+{
+  if(s==1)
+  {
+    document.getElementById("s3").style.display="inline";
+    return sss=0;
+  }
+  else
+  {
+    document.getElementById("s3").style.display="none";
+    return sss=1;
+  }
+}
