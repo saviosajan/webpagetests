95e97543cc7ee31c52cbedb2f28020d9ba7acbdc
saviosajan <74667758+saviosajan@users.noreply.github.com>
2021-09-19 23:02:46 +0530

    new1

--- script.js
+++ script.js
@@ -1,8 +1,54 @@
 var s;
 var ss;
 var sss;
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+var gta = new Audio();
+gta.src = "gta.mp3";
+var err = new Audio();
+err.src = "err.mp3";
+var fart = new Audio();
+fart.src = "fart.mp3";
+var wow = new Audio();
+wow.src = "wow.mp3";
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
 function ss1()
 {
+  err.play()
   if(s==1)
   {
     document.getElementById("s1").style.display="inline";
@@ -16,6 +62,8 @@ function ss1()
 }
 function ss2()
 {
+  fart.play()
+  fart.pause()
   if(s==1)
   {
     document.getElementById("s2").style.display="inline";
@@ -29,6 +77,7 @@ function ss2()
 }
 function ss3()
 {
+  gta.play()
   if(s==1)
   {
     document.getElementById("s3").style.display="inline";
@@ -40,3 +89,8 @@ function ss3()
     return sss=1;
   }
 }
+function ss4()
+{
+  wow.play()
+}
+
